506400f891a75a78f24abcfa19653ec76862b9b5
danie <daniel.j.mccarthy@mycit.ie>
2022-08-14 17:48:46 +0100

    Nav bar WIP failing build

--- app/src/main/java/com/example/barkingmadpetstorev2/SecondActivity.java
+++ app/src/main/java/com/example/barkingmadpetstorev2/SecondActivity.java
@@ -1,14 +1,25 @@
 package com.example.barkingmadpetstorev2;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.barkingmadpetstorev2.databinding.ActivityMainBinding;
+
 
 public class SecondActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shop);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        
+        
+        
+        
     }
 }
