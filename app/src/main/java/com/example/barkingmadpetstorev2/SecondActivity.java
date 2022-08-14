ba3e6404eda29f3351b0c4428106700b068dc84c
danie <daniel.j.mccarthy@mycit.ie>
2022-08-14 19:54:38 +0100

    Nav bar completed

--- app/src/main/java/com/example/barkingmadpetstorev2/SecondActivity.java
+++ app/src/main/java/com/example/barkingmadpetstorev2/SecondActivity.java
@@ -2,24 +2,58 @@ package com.example.barkingmadpetstorev2;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
-import com.example.barkingmadpetstorev2.databinding.ActivityMainBinding;
+
+import com.example.barkingmadpetstorev2.databinding.ActivityHomeBinding;
+
 
 
 public class SecondActivity extends AppCompatActivity {
-
-    ActivityMainBinding binding;
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
+            // case structure to determine selected nav bar item
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.shop:
+                    replaceFragment(new ShopFragment());
+                    break;
+                case R.id.basket:
+                    replaceFragment(new BasketFragment());
+                    break;
+
+
+
+            }
+
+            return true;
+        });
         
         
         
         
+    }
+
+    // Method for swapping out fragments
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
